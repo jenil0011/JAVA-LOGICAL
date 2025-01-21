12ed412a2f01839497c30b63ff44afb53463a0e2
jenil0011 <21bmiitbscit001@gmail.com>
2025-01-21 23:42:43 +0530

    third commit

---
+++ Pattern Programs/Pattern6.java
@@ -0,0 +1,24 @@
+/* print below pattern 
+
+0 0 0 0 0 5
+0 0 0 0 4
+0 0 0 3
+0 0 2
+0 1 
+       // here, we need two loops 1 for row and 1 for column
+ */
+
+public class Pattern6 {
+    public static void main(String[] args)
+    {
+        for(int i=5; i>=1 ; i--) // row loop
+        {
+            for(int j=1; j<=i ; j++)
+            {
+                System.out.print("0 ");
+            }
+            System.out.print((i+1)+"");
+            System.out.println("");
+        }
+    }
+}
